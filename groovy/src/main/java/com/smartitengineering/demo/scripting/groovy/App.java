11e58f117661fb64c38b4c6ffe447dc313b7dc18
kaisar <kaisar@smartitengineering.com>
2010-09-03 11:22:15 +0600

    Add support for groovy script.
    Signed-off-by: kaisar <kaisar@smartitengineering.com>

--- groovy/src/main/java/com/smartitengineering/demo/scripting/groovy/App.java
+++ groovy/src/main/java/com/smartitengineering/demo/scripting/groovy/App.java
@@ -1,13 +1,25 @@
 package com.smartitengineering.demo.scripting.groovy;
 
+import groovy.lang.Binding;
+import groovy.util.GroovyScriptEngine;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+  public static void main(String[] args) {
+    String[] roots = new String[]{"src/main/resources/"};
+    try {
+      GroovyScriptEngine engine = new GroovyScriptEngine(roots);
+      Binding binding = new Binding();
+      binding.setVariable("input", "SITEL");
+      engine.run("test.groovy", binding);
+      System.out.println(binding.getVariable("$output"));
     }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
